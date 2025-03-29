ed55feffcdfbbbbb47e9e8be614e2bcf6d16a9dd
jack tan <62325379+jacktan130802@users.noreply.github.com>
2025-03-29 21:05:38 +0800

    works login page

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import system.Login;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    //Add in attributes
+    List<String> userData;
+    public static void main(String[] args){
+        System.out.println("Welcome to BTO System! \nPlease Login");
+        new Main().run();
+    }
+
+    public void run(){
+        start();
+        runUntilQuit();
+        exit();
+    }
+
+    private void start(){
+        Login login = new Login();
+        userData = login.getData();
+    }
+
+    private void runUntilQuit(){
+        System.out.println("Getting Data...");
+        System.out.printf("Hello, %s, %s!\n", userData.get(0), userData.get(5));
+
+    }
+
+    private void exit(){
+        System.exit(0);
     }
 }
---
+++ src/system/Login.java
@@ -0,0 +1,67 @@
+package system;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Login {
+    String userID;
+    String password;
+    String[] userTypes = {"Applicant", "Manager", "Officer"};
+    ArrayList<String> userData = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    public Login() {
+        do {
+            System.out.println("Enter User ID:");
+            userID = sc.nextLine();
+            System.out.println("Enter Password:");
+            password = sc.nextLine();
+            System.out.println("Entered User ID: " + userID + ", Entered Password: " + password); // Debug statement
+            checkDatabase(userID, password);
+            if (userData.isEmpty())
+                System.out.println("Invalid User ID or Password!");
+        } while (userData.isEmpty());
+    }
+
+    private void checkDatabase(String userID, String password) {
+        File file = new File("src/user_data/ApplicantList.csv");
+        if (!file.exists()) {
+            System.out.println("ApplicantList.csv file not found at: " + file.getAbsolutePath());
+            return;
+        }
+
+        try {
+            int type = 0;
+            Scanner data = new Scanner(file);
+            data.nextLine(); // for skipping the title
+
+            while (data.hasNextLine()) { // Going through all user data
+                String line = data.nextLine();
+                String[] row = line.split(",");
+                System.out.println("Read line: " + line); // Debug statement
+                if (row.length == 1) {
+                    type++;
+                } else if (row.length == 5) {
+                    System.out.println("Checking user: " + row[1] + " with password: " + row[4]); // Debug statement
+                    if (row[1].equals(userID) && row[4].equals(password)) {
+                        Collections.addAll(userData, row); // adding basic details into userData
+                        userData.add(userTypes[type]); // adding type
+                        System.out.println("User found: " + userData); // Debug statement
+                        break; // Exit loop once user is found
+                    }
+                }
+            }
+            data.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getData() {
+        return userData;
+    }
+}
