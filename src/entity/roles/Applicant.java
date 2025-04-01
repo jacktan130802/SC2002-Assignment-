cb45d11dec968455b71f1fa45820d51af79f0d10
yyueyangg <cheryueyang@gmail.com>
2025-04-01 15:01:33 +0800

    Edit src/entity/roles/User and Applicant, need to implement Enquiry and Application class next as Applicant class has both Enquiry and Application

---
+++ src/entity/roles/Applicant.java
@@ -0,0 +1,112 @@
+package entity.roles;
+
+import entity.btoProject.BTOProject;
+import entity.enquiry.Enquiry;
+import enums.ApplicationStatus;
+import enums.FlatType;
+import enums.MaritalStatus;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Applicant extends User {
+
+    private Application application;
+    private List<Enquiry> enquiries = new ArrayList<>();
+    private int maxEnqId = 0;
+    private ApplicationStatus applicationStatus;
+    private FlatType flatType;
+
+    public Applicant(String NRIC, String password, int age, MaritalStatus maritalStatus) {
+        super(NRIC, password, age, maritalStatus);
+    }
+
+    public void projectView(BTOProject p) {
+        // Stub for future feature
+    }
+
+    public void projectApply(BTOProject p) {
+        // Stub for future feature
+    }
+
+    public void projectWithdraw(BTOProject p) {
+        // Stub for future feature
+    }
+
+    public void enquirySubmit(String message, BTOProject project) {
+        Enquiry e = new Enquiry(this, project, message);
+        enquiries.add(e);
+        System.out.println("Enquiry submitted with ID: " + e.getEnquiryID());
+    }
+
+    public void enquiryView() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter ID of enquiry you want to view: ");
+        int enqId = sc.nextInt();
+
+        boolean found = false;
+        for (Enquiry e : enquiries) {
+            if (e.getEnquiryID() == enqId) {
+                System.out.println("Message: " + e.getMessage());
+                System.out.println("Reply: " + (e.isReplied() ? e.getReply() : "[No reply yet]"));
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Could not find an enquiry with that ID!");
+        }
+    }
+
+    public void enquiryEdit(int enqId, String newMessage) {
+        for (Enquiry e : enquiries) {
+            if (e.getEnquiryID() == enqId) {
+                if (!e.isReplied()) {
+                    e.setMessage(newMessage);
+                    System.out.println("Enquiry updated.");
+                } else {
+                    System.out.println("Cannot edit enquiry. Already replied.");
+                }
+                return;
+            }
+        }
+        System.out.println("Enquiry ID not found.");
+    }
+
+    public void enquiryDelete(int enqId) {
+        Iterator<Enquiry> iter = enquiries.iterator();
+        while (iter.hasNext()) {
+            Enquiry e = iter.next();
+            if (e.getEnquiryID() == enqId) {
+                if (!e.isReplied()) {
+                    iter.remove();
+                    System.out.println("Enquiry deleted.");
+                } else {
+                    System.out.println("Cannot delete enquiry. Already replied.");
+                }
+                return;
+            }
+        }
+        System.out.println("Enquiry ID not found.");
+    }
+
+    public Application getApplication() {
+        return application;
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+
+    public List<Enquiry> getEnquiries() {
+        return enquiries;
+    }
+
+    public void displayMenu() {
+        System.out.println("--- Applicant Menu ---");
+        System.out.println("1. View Projects\n2. Apply for BTO\n3. View Application\n4. Submit/View/Edit/Delete Enquiries\n5. Withdraw Application");
+    }
+} 
---
+++ src/entity/roles/User.java
@@ -0,0 +1,50 @@
+package entity.user;
+
+
+import enums.MaritalStatus;
+
+public abstract class User{
+    // all attributes access level = protected, so subclasses can inherit
+    protected String NRIC;
+    protected String password;
+    protected int age;
+    protected MaritalStatus maritalStatus;
+
+    // constructor
+    public User(String NRIC, String password, int age, MaritalStatus maritalStatus) {
+        this.NRIC = NRIC;
+        this.password = password;
+        this.age = age;
+        this.maritalStatus = maritalStatus;
+    }
+
+    // setting the new password
+    public void changePassword(String newPassword) 
+    {
+        this.password = newPassword;
+    }
+
+    public boolean authenticate(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+
+
+    public abstract void displayMenu();
+    // not sure if needed, if login, logout and changing password is via login menu
+    // public boolean login(String NRIC,String loginPass) {
+    //     if (NRIC.equals(UserID)&&loginPass.equals(password)) {return true;}
+    //     else {return false;}
+    // }
+
+    // public void logout() {}
+    
+    // public void changePassword() {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter new password: ");
+    //     String newPassword = sc.next();
+        
+    //     this.password = newPassword;
+    //     System.out.println("Password successfully changed.");
+    //     // logout afterwards
+    // }
+}
