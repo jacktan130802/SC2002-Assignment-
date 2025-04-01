c14d07da4cb00e05b7991f59e48b839269566b77
yyueyangg <cheryueyang@gmail.com>
2025-04-01 15:35:06 +0800

    fix  src/entity/btoProject/BTOProject and  src/entity/roles/Applicant, need to edit  src/entity/roles/Officer and Manager next

--- src/entity/btoProject/BTOProject.java
+++ src/entity/btoProject/BTOProject.java
@@ -1,27 +1,134 @@
 package entity.btoProject;
 
+import entity.roles.HDBOfficer;
+import entity.roles.HDBManager;
 import java.time.LocalDate;
-import entity.user.Manager;
-import enums.FlatType;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BTOProject {
     private String projectName;
-    private String neighbourhood;
-    private FlatType flatType;
-    private int numUnits;
+    private String neighborhood;
+    private int twoRoomUnits;
+    private int threeRoomUnits;
+    private double priceTwoRoom;
+    private double priceThreeRoom;
     private LocalDate openingDate;
     private LocalDate closingDate;
-    private Manager managerInCharge;
-    private int availableOfficerSlots;
+    private HDBManager managerInCharge;
+    private boolean visibility;
+    private int maxOfficerSlots;
 
-    public BTOProject(String projectName, String neighbourhood, FlatType flatType, LocalDate openingDate, LocalDate closingDate, Manager managerInCharge) {
+    private List<HDBOfficer> registeredOfficers = new ArrayList<>();
+    private List<HDBOfficer> approvedOfficers = new ArrayList<>();
+
+    public BTOProject(String projectName, String neighborhood, int twoRoomUnits, double priceTwoRoom,
+                      int threeRoomUnits, double priceThreeRoom, LocalDate openingDate, LocalDate closingDate,
+                      HDBManager managerInCharge, int maxOfficerSlots) {
         this.projectName = projectName;
-        this.neighbourhood = neighbourhood;
-        this.flatType = flatType;
-        this.numUnits = 0;
+        this.neighborhood = neighborhood;
+        this.twoRoomUnits = twoRoomUnits;
+        this.priceTwoRoom = priceTwoRoom;
+        this.threeRoomUnits = threeRoomUnits;
+        this.priceThreeRoom = priceThreeRoom;
         this.openingDate = openingDate;
         this.closingDate = closingDate;
         this.managerInCharge = managerInCharge;
-        this.availableOfficerSlots = 10;    // Max 10
+        this.maxOfficerSlots = maxOfficerSlots;
+        this.visibility = false; // default
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public int getTwoRoomUnits() {
+        return twoRoomUnits;
+    }
+
+    public int getThreeRoomUnits() {
+        return threeRoomUnits;
+    }
+
+    public double getPriceTwoRoom() {
+        return priceTwoRoom;
+    }
+
+    public double getPriceThreeRoom() {
+        return priceThreeRoom;
+    }
+
+    public LocalDate getOpeningDate() {
+        return openingDate;
+    }
+
+    public LocalDate getClosingDate() {
+        return closingDate;
+    }
+
+    public HDBManager getManagerInCharge() {
+        return managerInCharge;
+    }
+
+    public boolean isVisible() {
+        return visibility;
+    }
+
+    public void setVisibility(boolean visibility) {
+        this.visibility = visibility;
+    }
+
+    public boolean isWithinApplicationPeriod(LocalDate date) {
+        return !(date.isBefore(openingDate) || date.isAfter(closingDate));
+    }
+
+    public List<HDBOfficer> getRegisteredOfficers() {
+        return registeredOfficers;
+    }
+
+    public List<HDBOfficer> getApprovedOfficers() {
+        return approvedOfficers;
+    }
+
+    public boolean registerOfficer(HDBOfficer officer) {
+        if (!registeredOfficers.contains(officer)) {
+            registeredOfficers.add(officer);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean approveOfficer(HDBOfficer officer) {
+        if (registeredOfficers.contains(officer) && !approvedOfficers.contains(officer)) {
+            if (approvedOfficers.size() < maxOfficerSlots) {
+                approvedOfficers.add(officer);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isOfficerApproved(HDBOfficer officer) {
+        return approvedOfficers.contains(officer);
+    }
+
+    public void updateFlatCount(String flatType) {
+        if (flatType.equalsIgnoreCase("2-Room")) {
+            twoRoomUnits--;
+        } else if (flatType.equalsIgnoreCase("3-Room")) {
+            threeRoomUnits--;
+        }
+    }
+
+    public boolean hasTwoRoom() {
+        return twoRoomUnits > 0;
+    }
+
+    public boolean hasThreeRoom() {
+        return threeRoomUnits > 0;
     }
 }
