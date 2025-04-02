c116db386453fa3a4230b9d02043d5ab95738413
yyueyangg <cheryueyang@gmail.com>
2025-04-02 14:26:22 +0800

    Minor edits

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,6 @@ public class Main {
     public static void main(String[] args) {
         // Load data
         Database.loadAll();
-
         // Controllers
         LoginAuthController loginCtrl = new LoginAuthController(Database.getUsers());
         ApplicationController appCtrl = new ApplicationController();
