853bfafacacb65703988fedb19f846ee2d6cd1fa
jack tan <62325379+jacktan130802@users.noreply.github.com>
2025-04-22 21:13:37 +0800

    add exceptions for main .java

--- src/Main.java
+++ src/Main.java
@@ -35,67 +35,75 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("===== BTO Management System =====");
-            System.out.println("1. Login");
-            System.out.println("2. Change Password");
-            System.out.println("3. Exit");
-            System.out.print("Choose option: ");
-            int opt = sc.nextInt();
-            sc.nextLine();
+            try {
+                System.out.println("===== BTO Management System =====");
+                System.out.println("1. Login");
+                System.out.println("2. Change Password");
+                System.out.println("3. Exit");
+                System.out.print("Choose option: ");
+                int opt = sc.nextInt();
+                sc.nextLine();
 
-            if (opt == 3) {
-                Database.saveAll();
-                System.out.println("All data saved. Goodbye.");
-                break;
-            }
-
-            if (opt == 1) {
-                String[] login = loginMenu.displayLoginPrompt();
-                if (login == null) {
-                    // Just go back to the main menu
-                    System.out.println(); 
-                    continue; 
-                }
-                if (!NRICValidator.isValidNRIC(login[0])) {
-                    System.out.println("Invalid NRIC format.\n");
-                    continue;
-                }
-                User currentUser = loginCtrl.verifyLogin(login[0], login[1]);
-                if (currentUser == null) {
-                    System.out.println("Invalid credentials.\n");
-                    continue;
-                }
-                mainMenu.displayMainMenu(currentUser);
-
-                if (currentUser instanceof Applicant && !(currentUser instanceof HDBOfficer)) {
-                    ApplicantController.run((Applicant) currentUser, appCtrl, projectCtrl, enquiryCtrl, applicantMenu, logoutMenu, sc);
-                } else if (currentUser instanceof HDBOfficer) {
-                    OfficerController.run((HDBOfficer) currentUser, projectCtrl, enquiryCtrl, receiptCtrl, officerMenu, logoutMenu, sc, appCtrl);
-                } else if (currentUser instanceof HDBManager) {
-                    ManagerController.run((HDBManager) currentUser, managerMenu,logoutMenu, appCtrl, enquiryCtrl, regCtrl, sc);
-                }
-            } else if (opt == 2) {
-                System.out.print("Enter NRIC (E.g S1234567D): ");
-                String nric = sc.nextLine();
-                if (!NRICValidator.isValidNRIC(nric)) {
-                    System.out.println("Invalid NRIC format.\n");
-                    continue;
-                }
-                User user = Database.getUsers().get(nric);
-                if (user == null) {
-                    System.out.println("User not found.\n");
-                    continue;
-                }
-                String[] pwds = loginMenu.promptChangePassword();
-                if (!user.authenticate(pwds[0])) {
-                    System.out.println("Incorrect current password.\n");
-                    continue;
-                } else {
-                    user.changePassword(pwds[1]);
-                    System.out.println("Password changed successfully.\n");
+                if (opt == 3) {
                     Database.saveAll();
+                    System.out.println("All data saved. Goodbye.");
+                    break;
                 }
+
+                if (opt == 1) {
+                    String[] login = loginMenu.displayLoginPrompt();
+                    if (login == null) {
+                        // Just go back to the main menu
+                        System.out.println();
+                        continue;
+                    }
+                    if (!NRICValidator.isValidNRIC(login[0])) {
+                        System.out.println("Invalid NRIC format.\n");
+                        continue;
+                    }
+                    User currentUser = loginCtrl.verifyLogin(login[0], login[1]);
+                    if (currentUser == null) {
+                        System.out.println("Invalid credentials.\n");
+                        continue;
+                    }
+                    mainMenu.displayMainMenu(currentUser);
+
+                    if (currentUser instanceof Applicant && !(currentUser instanceof HDBOfficer)) {
+                        ApplicantController.run((Applicant) currentUser, appCtrl, projectCtrl, enquiryCtrl, applicantMenu, logoutMenu, sc);
+                    } else if (currentUser instanceof HDBOfficer) {
+                        OfficerController.run((HDBOfficer) currentUser, projectCtrl, enquiryCtrl, receiptCtrl, officerMenu, logoutMenu, sc, appCtrl);
+                    } else if (currentUser instanceof HDBManager) {
+                        ManagerController.run((HDBManager) currentUser, managerMenu, logoutMenu, appCtrl, enquiryCtrl, regCtrl, sc);
+                    }
+                } else if (opt == 2) {
+                    System.out.print("Enter NRIC (E.g S1234567D): ");
+                    String nric = sc.nextLine();
+                    if (!NRICValidator.isValidNRIC(nric)) {
+                        System.out.println("Invalid NRIC format.\n");
+                        continue;
+                    }
+                    User user = Database.getUsers().get(nric);
+                    if (user == null) {
+                        System.out.println("User not found.\n");
+                        continue;
+                    }
+                    String[] pwds = loginMenu.promptChangePassword();
+                    if (!user.authenticate(pwds[0])) {
+                        System.out.println("Incorrect current password.\n");
+                        continue;
+                    } else {
+                        user.changePassword(pwds[1]);
+                        System.out.println("Password changed successfully.\n");
+                        Database.saveAll();
+                    }
+                }
+            }catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                sc.nextLine(); // Clear the invalid input
+            } catch (Exception e) {
+                System.out.println("An error occurred: " + e.getMessage());
             }
+
         }
     }
 
